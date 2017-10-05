210706b22a7b72e9e870123dfa5b84385c8afda1
Cpruit96 <pruitt_chandler@columbussate.edu>
2017-10-05 16:18:51 +0000

    first commit

--- index.php
+++ index.php
@@ -1,74 +1,114 @@
 <?php
     require_once('database.php');
     
-    $customerID = 1008;
-    $query = "SELECT firstName, lastName, address, city, state FROM customers WHERE customerID = ?";
+    $state = "CA";
+    $query = "SELECT firstName, lastName, city FROM customers WHERE state = ? Order By lastName";
     
     $stmt = $conn->prepare($query);
-    $stmt->bind_param('s', $customerID);
+    $stmt->bind_param('s', $state);
     $stmt->execute();
     $stmt->store_result();
-    $stmt->bind_result($firstName, $lastName, $address, $city, $state);
+    $stmt->bind_result($firstName, $lastName, $city);
     
-    while($stmt->fetch()) {
-        echo "First: ".$firstName;
-        echo "<br />Last: ".$lastName;
-        echo "<br />Address: ".$address;
-        echo "<br />City: ".$city;
-        echo "<br />State: ".$state;
-    }
-    $stmt->free_result();
-    mysqli_close($conn);
-
+    
+    $version = 2.0;
+    $query2 = "SELECT productCode, name FROM products WHERE version = ?";
+    
+    $stmt2 = $conn->prepare($query2);
+    $stmt2->bind_param('s', $version);
+    $stmt2->execute();
+    $stmt2->store_result();
+    $stmt2->bind_result($productCode, $name);
+    
+   
+   
    
 ?>
-<!--
+
 <!DOCTYPE html>
 <html>
 
 <!-- the head section -->
-<!--<head>
+<head>
     <title>Cusomter Relations</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
-<!--<body>
+<body>
     <div id="page">
 
     <div id="header">
-        <h1>Customers</h1>
+        <h1>Customers and Products</h1>
     </div>
 
     <div id="main">
 
-        <h1>Customer List</h1>
-
+        
        
 
         <div id="content">
-            <!-- display a table of products -->
+            <!-- display a table of customers -->
             
-            <!--<table>
+            <table>
                 <tr>
                     <th>First Name</th>
                     <th>Last Name</th>
+                    <th>City</th>
                 </tr>
+                    <?php 
+                    while ($stmt->fetch()){ ?>
+                        <tr> 
+                            <td><?php echo $firstName; ?></td>
+                            <td><?php echo $lastName; ?></td>
+                            <td><?php echo $city; ?></td>
+                        </tr>
+                    
+                        
+                    <?php } ?>
                 
+                
+            </table>
+            
+        </div>
+        
+        
+        <div id="content">
+            <!-- display a table of customers -->
+            
+            <table>
                 <tr>
-                    <td>?></td>
+                    <th>Product Code</th>
+                    <th>Product Name</th>
                 </tr>
+                    <?php 
+                    while ($stmt2->fetch()){ ?>
+                        <tr> 
+                            <td><?php echo $productCode; ?></td>
+                            <td><?php echo $name; ?></td>
+                        </tr>
+                    
+                        
+                    <?php } ?>
+                
                 
             </table>
             
         </div>
     </div>
-
+    
+    
+    
     <div id="footer">
         <p>&copy;  Columnus State University</p>
     </div>
 
     </div><!-- end page -->
-<!--</body>
+    
+    <?php
+     $stmt->free_result();
+    mysqli_close($conn);
+    ?>
+</body>
 </html>
--->
+>
